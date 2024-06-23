5e8b36415e148b980a317971dd8234194debd800
NicolasSP90 <nicolassp90@hotmail.com>
2024-06-23 00:50:57 -0300

    finalizado módulo 2.1.4 - Boas práticas de programação

--- 2. Iniciante em Programação/01. Iniciando com JavaScript/Projeto_Lógica_JS/app.js
+++ 2. Iniciante em Programação/01. Iniciando com JavaScript/Projeto_Lógica_JS/app.js
@@ -1,6 +1,7 @@
 alert("Bem Vindo ao jogo do Número Secreto");
 
-let numeroSecreto = 5;
+let rangeMaximo = 100;
+let numeroSecreto = Math.floor((Math.random()*rangeMaximo) + 1); // método Math.random retorna de 0 (incluido) a 1 (excluido). Ao aidicionar 1, retorna de 1 até o número indicado.
 let numeroEscolhido;
 let tentativas = 1;
 console.log(numeroSecreto);
@@ -8,10 +9,10 @@ console.log(numeroSecreto);
 // Mensagem de acerto do número
 while (numeroSecreto != numeroEscolhido) {
     
-    numeroEscolhido = prompt("Escolha um número entre 1 e 10");
+    numeroEscolhido = prompt(`Escolha um número entre 1 e ${rangeMaximo}`);
 
     if (numeroSecreto == numeroEscolhido) {
-        alert(`Acertou! Número ${numeroSecreto}, em ${tentativas} tentativas.`);
+        break;
     } else {
         if (numeroSecreto > numeroEscolhido) {
             alert(`O Número Secreto é maior que ${numeroEscolhido}.`);
@@ -21,3 +22,12 @@ while (numeroSecreto != numeroEscolhido) {
         tentativas ++ // tentativas = tentativas + 1;
     }
 }
+let palavraTentativa = (tentativas > 1) ?  "tentativas" : "tentativa"
+
+// if (tentativas == 1) {
+//     alert(`Acertou! Número ${numeroSecreto}, em ${tentativas} tentativa.`);
+// } else {
+//    alert(`Acertou! Número ${numeroSecreto}, em ${tentativas} tentativas.`);
+//}
+alert(`Acertou! Número ${numeroSecreto}, em ${tentativas} ${palavraTentativa}.`);
+
