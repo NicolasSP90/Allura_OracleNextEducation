9c4bcdc2782da9f7ec1bb7071e5f968f8f0c5083
NicolasSP90 <nicolassp90@hotmail.com>
2024-07-14 17:06:54 -0300

    Iniciada estilização em conjunto com elementos JS para criptografia

---
+++ 2. Iniciante em Programação/10.  Praticando lógica de programação Challenge Decodificador de Texto/app.js
@@ -0,0 +1,9 @@
+let textoNomal = "";
+
+function criptografar() {
+    textoNomal = document.getElementById("input").value;
+    console.log(textoNomal);
+
+    document.getElementById("hidden_visual").href = "styles/style_answer.css";
+
+}
